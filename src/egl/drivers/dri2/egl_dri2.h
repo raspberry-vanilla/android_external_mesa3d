3abcee1101292c25eb5df18e92ceb162ef668d64
Yiwei Zhang <zzyiwei@chromium.org>
2022-08-23 21:41:47 +0000

    egl/android: use 64bit gralloc usage and respect BufferUsage::FRONT_BUFFER
    This chagne also removes some redundant api level check.
    
    Change-Id: I7ccfc68f71e1ef95f632b48dd09ffb934a213f34
    Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>

--- src/egl/drivers/dri2/egl_dri2.h
+++ src/egl/drivers/dri2/egl_dri2.h
@@ -312,8 +312,8 @@ struct dri2_egl_display {
 
 #ifdef HAVE_ANDROID_PLATFORM
    const gralloc_module_t *gralloc;
-   /* gralloc vendor usage bit for front rendering */
-   uint32_t front_rendering_usage;
+   /* gralloc usage bit for front rendering */
+   uint64_t front_rendering_usage;
 #endif
 };
 
@@ -400,7 +400,7 @@ struct dri2_egl_surface {
       struct ANativeWindowBuffer *buffer;
       int age;
    } *color_buffers, *back;
-   uint32_t gralloc_usage;
+   uint64_t gralloc_usage;
 #endif
 
    /* surfaceless and device */
