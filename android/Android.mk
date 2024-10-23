8b5c50cddb51eabafd230035159b06d9d55be595
Yumi Yukimura <me.cafebabe@gmail.com>
2024-10-24 00:44:49 +0800

    android: Define a phony package that includes all defined mesa3d libs as dependency
    Normally, all defined libs are needed for a target that uses mesa3d
    and we have to manually add them to `PRODUCT_PACKAGES`. There
    shouldn't be a module that's defined but unneeded.
    
    After this change, we could build all of them automatically just by
    including `mesa3d` in `PRODUCT_PACKAGES`.

--- android/Android.mk
+++ android/Android.mk
@@ -133,11 +133,15 @@ endif
 
 #-------------------------------------------------------------------------------
 
+MESA3D_LIBS :=
+
 # $1: name
 # $2: subdir
 # $3: source prebuilt
 # $4: export headers
 define mesa3d-lib
+MESA3D_LIBS += $1
+
 include $(CLEAR_VARS)
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE := $1
@@ -191,6 +195,12 @@ $(eval $(call mesa3d-lib,$(MESA_LIBGBM_NAME),,MESA3D_LIBGBM_BIN,$(MESA3D_TOP)/sr
 # Module 'dri_gbm', produces '/vendor/lib{64}/dri_gbm.so'
 $(eval $(call mesa3d-lib,dri_gbm,,MESA3D_DRI_GBM_BIN))
 endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := mesa3d
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := $(MESA3D_LIBS)
+include $(BUILD_PHONY_PACKAGE)
 
 #-------------------------------------------------------------------------------
 
